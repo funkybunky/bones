0097c24c3b29cfbbac6e942f8f52e074395262ef
Marcus Kleppe <marcuskleppe@gmail.com>
2017-03-25 13:50:01 +0100

    use app container

--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
-import App from './components/app.jsx';
+import App from './components/app_container.jsx';
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.getElementById('app'));
