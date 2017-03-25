7f1a3c68492715c19e8d2e7e032b71cd8545dd78
Marcus Kleppe <marcuskleppe@gmail.com>
2017-03-25 12:53:57 +0100

    use controlled component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 
 export default class App extends React.Component {
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     inputText: ""
+  //   };
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleSend = this.handleSend.bind(this);
+  // }
+
+  state = {
+    inputText: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+
+  handleSend = (event) => {
+
+  }
+
   render() {
     return (
       <div>
         <h1>Hello React</h1>
         <p>JSX ist sehr ähnlich zu HTML</p>
-        <input type='text' />
-        <button>Send</button>
+        <input type="text" value={this.state.inputText} onChange={this.handleChange} />
+        <button onClick={this.handleSend}>Send</button>
       </div>
     )
   }
