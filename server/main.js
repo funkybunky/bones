d8a2f42b24727b2bc3c009da9b97efc6a634bb02
Marcus Kleppe <marcuskleppe@gmail.com>
2017-03-25 13:23:02 +0100

    add chat collection and method

---
+++ server/main.js
@@ -0,0 +1,12 @@
+import { Meteor } from 'meteor/meteor'
+
+const Chat = new Mongo.Collection('chat')
+
+Meteor.methods({
+  addChatMessage: (message) => {
+    check(message, String)
+    Chat.insert({
+      message: message
+    })
+  }
+})
