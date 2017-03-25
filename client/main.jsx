5aa9f059426232274da5576c23f662dab35620cb
Marcus Kleppe <marcuskleppe@gmail.com>
2017-03-25 12:03:38 +0100

    separate files

--- client/main.jsx
+++ client/main.jsx
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>Hello React</p>
-      </div>
-    )
-  }
-}
+import App from './components/app.jsx';
 
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.getElementById('app'));
