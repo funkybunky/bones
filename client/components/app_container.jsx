c26b3f6f16a3a1be6ec7df9673d692b124d9e6ca
Marcus Kleppe <marcuskleppe@gmail.com>
2017-03-25 13:43:19 +0100

    add container for app

---
+++ client/components/app_container.jsx
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor'
+import { Chat } from './both/collections.js'
+import { createContainer } from 'meteor/react-meteor-data'
+
+import App from './app.jsx'
+
+export default createContainer(() => {
+  const chatHandle = Meteor.subscribe('chat.all')
+
+  return {
+    ready: chatHandle.ready(),
+    chatMessages: Chat.find().fetch()
+  }
+}, App)
